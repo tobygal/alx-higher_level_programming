ee64ec7cd0fd158ff1402f41fb72988a6daafa81
tobygal <tochiazolike@yahoo.com>
2022-06-05 06:27:04 -0700

    Amend print-interger

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,4 +3,4 @@
 def print_list_integer(my_list=[]):
     count = len(my_list)
     for i in range(count):
-        print("{}".format(my_list[i], end=""))
+        print("{:d}".format(my_list[i], end=""))
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     count = len(my_list)
     my_list.reverse()
     for i in range(count):
-        print("{}".format(my_list[i], end=""))
+        print("{:d}".format(my_list[i], end=""))
