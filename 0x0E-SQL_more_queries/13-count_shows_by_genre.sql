6ff6283c96a88a6e162f7f9c660dc021fe935118
tobygal <tochiazolike@yahoo.com>
2022-08-10 19:37:10 -0700

    Remove alias

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- a script that lists all genres from hbtn_0d_tvshows with its count
-SELECT tv_gen.name as genre, COUNT(show_gen.show_id) as number_of_shows
-FROM tv_genres as tv_gen
-INNER JOIN tv_show_genres as show_gen
-ON tv_gen.id = show_gen.genre_id
+SELECT tv_genres.name as genre, COUNT(tv_show_genres.show_id) as number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,9 @@
 -- a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
-SELECT tv_gen.name as name
-FROM tv_genres as tv_gen
-JOIN tv_show_genres as show_gen
-ON tv_gen.id = show_gen.genre_id
-JOIN tv_shows as shows
-ON shows.id = show_gen.show_id
-WHERE shows.title = "Dexter"
-ORDER BY name ASC;
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;
