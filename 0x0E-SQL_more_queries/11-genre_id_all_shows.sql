3d3a0bc9e23995dc19fee3e3b8e5dc9e0b3aa8dc
tobygal <tochiazolike@yahoo.com>
2022-08-10 19:28:35 -0700

    update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
--- a script that lists all shows contained in the database hbtn_0d_tvshows
-SELECT shows.title, show_gen.genre_id
-FROM tv_shows as shows
-LEFT OUTER JOIN tv_show_genres as show_gen
-ON shows.id = show_gen.show_id
-ORDER BY shows.title, show_gen.genre_id;
+-- a script that lists all shows contained in the database hbtn_0d_tvshowis
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
