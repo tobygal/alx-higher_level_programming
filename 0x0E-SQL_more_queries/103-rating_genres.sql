472414fc30f85ea1bdf30348cd00fb84966075c6
tobygal <tochiazolike@yahoo.com>
2022-08-11 21:12:23 -0700

    uppercase fix

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,5 @@
 -- list all genre in db hbtn_0d_tvshows_rate by their rating
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) as rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
