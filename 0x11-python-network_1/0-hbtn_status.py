cfdca4940eb02841f47cd3af3ffe69d2281fb793
tobygal <tochiazolike@yahoo.com>
2022-09-23 21:42:48 -0700

    Correct task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,10 @@
 """a py script that fetches https://alx-intranet.hbtn.io/status"""
 
 
-import urllib.request
+from urllib import request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
